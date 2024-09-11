0a8c1192ae4457e668e51229218ecbb76d549192
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 01:51:51 +0300

    sql advansed

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,7 @@
+-- valid_email.sql
+CREATE TRIGGER valid_email BEFORE UPDATE ON users FOR EACH ROW
+    BEGIN
+        IF NEW.email != OLD.email THEN
+            SET NEW.valid_email = 0;
+        END IF;
+    END;
