df5056141d19cef507b38ef86bf66d9415fd0c04
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 02:37:02 +0300

    sql advansed

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,8 +8,9 @@ BEGIN
     SELECT SUM(corrections.score * projects.weight) INTO total_score,
             SUM(projects.weight) INTO total_count
     FROM corrections
-    JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id AND projects.name = project_name;
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
 
     UPDATE users SET users.average_score = total_score / total_count
     WHERE users.id = user_id;
