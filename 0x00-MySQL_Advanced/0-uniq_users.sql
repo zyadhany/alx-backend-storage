bd43f07ded44bba687f6ea960ff7d036d122506d
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 01:23:36 +0300

    sql advansed

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+/*
+ * This code performs a selection operation.
+ * It retrieves data from a table based on specified conditions.
+ */
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
