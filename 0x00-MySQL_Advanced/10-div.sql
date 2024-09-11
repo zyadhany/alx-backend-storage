aabb74a98d4ad9dc12fb5fa491b7064a76bc72e1
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 02:24:32 +0300

    sql advansed

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -4,7 +4,7 @@ CREATE FUNCTION SafeDiv(a INT, b INT)
 BEGIN
     DECLARE res float DEFAULT 0;
     if b != 0 THEN
-        res = a / b;
+        SET res = a / b;
     END IF;
     RETURN res;
 END; //
