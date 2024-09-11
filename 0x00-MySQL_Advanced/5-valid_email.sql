b4819d7aab51baead1e97adcc5d975d876359001
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 02:00:51 +0300

    sql advansed

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,9 @@
 -- valid_email.sql
+DELIMITER $$
 CREATE TRIGGER resets_valid_email BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
     IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
-END;
+END; $$
+DELIMITER ;
