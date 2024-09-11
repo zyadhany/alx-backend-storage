6bbdbb9f506451768d19844845ea632564c29dde
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 02:08:27 +0300

    sql advansed

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,15 @@
+-- procedure AddBonus
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
+BEGIN
+    DECLARE project_id INT;
+    DECLARE project_cound INT;
+
+    SELECT COUNT(*) INTO project_cound FROM projects WHERE name = project_name;
+
+    if project_cound = 0 THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END;
