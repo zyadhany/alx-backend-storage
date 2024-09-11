9ae20282df6f307715fc2811ea4921c1d7a6df43
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 02:14:39 +0300

    sql advansed

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,7 +7,7 @@ BEGIN
 
     SELECT SUM(score), COUNT(*) INTO total_score, total_count
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
 
     UPDATE users SET users.average_score = total_score / total_count
     WHERE users.id = user_id;
