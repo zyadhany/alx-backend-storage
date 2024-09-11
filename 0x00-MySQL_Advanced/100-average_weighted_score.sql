629a14321ac72fd957dbe1688259e080ded6b7d7
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 02:42:38 +0300

    sql advansed

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,11 +5,19 @@ BEGIN
     DECLARE total_score INT;
     DECLARE total_count INT;
 
-    SELECT SUM(corrections.score * projects.weight) INTO total_score,
-            SUM(projects.weight) INTO total_count
-    FROM corrections
-    JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id AND projects.name = project_name;
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    SELECT SUM(projects.weight)
+        INTO total_count
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
 
     UPDATE users SET users.average_score = total_score / total_count
     WHERE users.id = user_id;
