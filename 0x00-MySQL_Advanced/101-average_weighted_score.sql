91acf963207fb38b06fd3daf152ba5b593d93c8c
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 02:58:51 +0300

    sql advansed

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,48 +1,32 @@
 -- Procedure: ComputeAverageWeightedScoreForUsers
 DELIMITER $$
-
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE done INT DEFAULT 0;
-    DECLARE user_id INT;
-    DECLARE total_score INT;
-    DECLARE total_count INT;
+    ALTER TABLE users ADD COLUMN total_score;
+    ALTER TABLE users ADD COLUMN num_proect;
 
-    DECLARE user_cursor CURSOR FOR
-    SELECT id FROM user;
-
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-
-    OPEN user_cursor;
-
-    read_loop: LOOP
-        FETCH user_cursor INTO user_id;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-
+    UPDATE users
+    SET total_score = (
         SELECT SUM(corrections.score * projects.weight)
-            INTO total_score
             FROM corrections
                 INNER JOIN projects
                     ON corrections.project_id = projects.id
-            WHERE corrections.user_id = user_id;
-
-        SELECT SUM(projects.weight)
-            INTO total_count
-            FROM corrections
-                INNER JOIN projects
-                    ON corrections.project_id = projects.id
-            WHERE corrections.user_id = user_id;
-
-        IF total_count IS NOT NULL AND total_count != 0 THEN
-            UPDATE user SET user.average_score = total_score / total_count
-            WHERE user.id = user_id;
-        END IF;
-    END LOOP;
-
-    CLOSE user_cursor;
-    DEALLOCATE PREPARE user_cursor;
+            WHERE corrections.user_id = users.id
+            );
+    
+    UPDATE users
+        SET num_proect = (
+            SELECT SUM(projects.weight)
+                FROM corrections
+                    INNER JOIN projects
+                        ON corrections.project_id = projects.id
+                WHERE corrections.user_id = users.id
+            );
+    
+    UPDATE users
+        SET average_score = total_score / num_proect;
+    
+    ALTER TABLE users DROP COLUMN total_score;
+    ALTER TABLE users DROP COLUMN num_proect;
 END $$
-
 DELIMITER ;
