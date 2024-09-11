22cd2cdc754c85af8ad1e25d85cec5492225230b
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 02:37:50 +0300

    sql advansed

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,9 +5,16 @@ BEGIN
     DECLARE total_score INT;
     DECLARE total_count INT;
 
-    SELECT SUM(corrections.score * projects.weight) INTO total_score,
-            SUM(projects.weight) INTO total_count
-    FROM corrections
+   SELECT SUM(corrections.score * projects.weight)
+        INTO total_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    SELECT SUM(projects.weight)
+        INTO total_count
+        FROM corrections
             INNER JOIN projects
                 ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
