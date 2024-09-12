f28abdf10c465acef5d4d6d311dddad65bcf124b
zyadhany <zeyad.hany2003@gmail.com>
2024-09-12 03:00:21 +0300

    sql advansed

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,13 +1,13 @@
 -- Procedure: ComputeAverageWeightedScoreForUsers
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-    ALTER TABLE users ADD COLUMN total_score;
-    ALTER TABLE users ADD COLUMN num_proect;
+    ALTER TABLE users ADD COLUMN total_score INT NOT NULL;
+    ALTER TABLE users ADD COLUMN num_proect INT NOT NULL;
 
     UPDATE users
     SET total_score = (
-        SELECT SUM(corrections.score * projects.weight)
+            SELECT SUM(corrections.score * projects.weight)
             FROM corrections
                 INNER JOIN projects
                     ON corrections.project_id = projects.id
@@ -22,9 +22,9 @@ BEGIN
                         ON corrections.project_id = projects.id
                 WHERE corrections.user_id = users.id
             );
-    
+
     UPDATE users
-        SET average_score = total_score / num_proect;
+        SET users.average_score = users.total_score / users.num_proect;
     
     ALTER TABLE users DROP COLUMN total_score;
     ALTER TABLE users DROP COLUMN num_proect;
