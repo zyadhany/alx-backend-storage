017dff6a4ce968351d662788dd71724e91a6bfea
zyadhany <zeyad.hany2003@gmail.com>
2024-09-13 21:32:32 +0300

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,18 +7,25 @@ import requests
 import redis
 
 
+def cache(method):
+    """ Cache decorator """
+    def wrapper(self, *args):
+        """ Wrapper function """
+        re = redis.Redis()
+        key = f"count:{args[0]}"
+        res_key = f"result:{args[0]}"
+
+        re.incr(key)
+        res = re.get(res_key)
+        if res:
+            return res.decode('utf-8')
+        res = method(self, args[0])
+        re.set(key, 0)
+        re.setex(res_key, 10, res)
+        return res
+    return wrapper
+
+
+@cache
 def get_page(url: str) -> str:
-    """ Get page """
-    re = redis.Redis()
-    key = f"count:{url}"
-    res_key = f"result:{url}"
-
-    re.incr(key)
-    res = re.get(res_key)
-    if res:
-        return res.decode('utf-8')
-    res = requests.get(url).text
-    re.set(key, 0)
-    re.setex(res_key, 10, res)
-    return res
-
+    return requests.get(url).text
