77493a6a0b6a26bec086b4bc768cd951a7c37cee
zyadhany <zeyad.hany2003@gmail.com>
2024-09-13 21:35:33 +0300

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,21 +9,20 @@ import redis
 
 def cache(method):
     """ Cache decorator """
-    def wrapper(self, *args):
-        """ Wrapper function """
+    def invoker(url):
+        """ invoker function """
         re = redis.Redis()
-        key = f"count:{args[0]}"
-        res_key = f"result:{args[0]}"
+        key = f'count:{url}'
 
         re.incr(key)
-        res = re.get(res_key)
+        res = re.get(url)
         if res:
             return res.decode('utf-8')
-        res = method(self, args[0])
+        res = method(url)
         re.set(key, 0)
-        re.setex(res_key, 10, res)
+        re.setex(url, 10, res)
         return res
-    return wrapper
+    return invoker
 
 
 @cache
