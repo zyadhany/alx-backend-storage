214b037cc9e092ab183264c839dab2a86d1be532
zyadhany <zeyad.hany2003@gmail.com>
2024-09-13 22:11:12 +0300

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,15 +6,30 @@ web file
 import requests
 import redis
 from functools import wraps
+from typing import Callable
 
 
+redis_store = redis.Redis()
+""" Redis store """
+
+
+def casher(method: Callable) -> Callable:
+    """ Casher decorator """
+    @wraps(method)
+    def wrapper(url) -> str:
+        """ Wrapper function """
+        redis_store.incr(f"count:{url}")
+        res = redis_store.get(f"result:{url}")
+        if res:
+            return res.decode('utf-8')
+        res = method(url)
+        redis_store.setex(f"result:{url}", 10, res)
+        redis_store.set(f"count:{url}", 0)
+        return res
+    return wrapper
+
+
+@casher
 def get_page(url: str) -> str:
     """ Get page function """
-    re = redis.Redis()
-    re.incr(f"count:{url}")
-    res = re.get(url)
-    if res:
-        return res.decode('utf-8')
-    res = requests.get(url).text
-    re.setex(url, 10, res)
-    return res
+    return requests.get(url).text
