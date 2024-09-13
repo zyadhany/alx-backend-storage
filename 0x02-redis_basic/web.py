84f556fc7f218073640431ffcb33b3849752c583
zyadhany <zeyad.hany2003@gmail.com>
2024-09-13 21:25:52 +0300

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,8 +12,7 @@ def get_page(url: str) -> str:
     r = redis.Redis()
     key = f"count:{url}"
     r.incr(key)
-    if r.get(key) and int(r.get(key)) % 10 == 0:
-        r.setex(f"cached:{url}", 10, requests.get(url).text)
+    r.setex(f"cached:{url}", 10, requests.get(url).text)
     if r.get(f"cached:{url}"):
         return r.get(f"cached:{url}").decode('utf-8')
     return requests.get(url).text
