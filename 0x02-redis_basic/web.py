f2176476114494a140a3b973b939bf2aabeccfc9
zyadhany <zeyad.hany2003@gmail.com>
2024-09-13 22:13:32 +0300

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -17,15 +17,15 @@ def casher(method: Callable) -> Callable:
     """ Casher decorator """
     @wraps(method)
     def invoker(url) -> str:
-        """ invoker function """
-        redis_store.incr(f"count:{url}")
-        res = redis_store.get(f"result:{url}")
-        if res:
-            return res.decode('utf-8')
-        res = method(url)
-        redis_store.setex(f"result:{url}", 10, res)
-        redis_store.set(f"count:{url}", 0)
-        return res
+        """ wrapped"""
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
     return invoker
 
 
