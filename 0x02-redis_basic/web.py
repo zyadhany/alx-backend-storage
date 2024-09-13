576d129e18c70809abc4cc59fbc2e562a4c29219
zyadhany <zeyad.hany2003@gmail.com>
2024-09-13 21:47:43 +0300

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,26 +8,13 @@ import redis
 from functools import wraps
 
 
-def cache(method):
-    """ Cache decorator """
-    @wraps(method)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = method(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return wrapper
 
-
-@cache
 def get_page(url: str) -> str:
     """ Get page function """
+    re = redis.Redis()
+    re.incr(f"count:{url}")
     return requests.get(url).text
+
+
+if __name__ == "__main__":
+    get_page("http://google.com")
